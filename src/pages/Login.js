b392066bd8b5d2d828362482701ad4edac5fd279
danhhuynh25029 <danhhuynh250@gmail.com>
2022-09-19 07:09:14 +0700

    feature: updae login page

---
+++ src/pages/Login.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Button, Checkbox, Form, Input, Layout,Menu } from "antd";
+import { Col, Row } from "antd";
+import { Header,Content } from "antd/lib/layout/layout";
+
+const Login = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Layout>
+      <Header><div className="logo" />
+        <Menu theme="dark" mode="horizontal" />
+      </Header>
+      <Content>
+        <Row>
+          <Col span={6}></Col>
+          <Col span={12}>
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col span={6}></Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
+};
+
+export default Login;
